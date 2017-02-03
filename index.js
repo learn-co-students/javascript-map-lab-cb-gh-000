59df4801e10908c16f847db4da10010a86e9648c
Qin LIN <linq.sophie@gmail.com>
2017-02-03 08:02:00 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {
+    alliance : knownDecepticons.includes(robot.name)? 'decepticon':'autobot'
+  })
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +27,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var coloredZebraStripes = zebraStripes.map(function (stripe) {
+  var even = zebraStripes.indexOf(stripe) % 2;
+  return Object.assign({}, stripe, {
+    color: even ? 'white' : 'black'
+  })
+})
