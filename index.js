9f083a8ece56fa7c6f8db8ff93323d7ca0c5fe7d
Justin Ackerman <jackerman91@gmail.com>
2017-07-12 05:11:06 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+/*
+* Create a new variable called `sortedRobots`;
+* The value of this variable is the mapped `robots` array;
+* Return each robot object with the `alliance` filled in with either an `'autobot'` or `'decepticon'` string.
+* To figure out a robot's alliance, check if their name exists in the `knownDecepticons` array.
+* (Hint: use `Array.prototype.includes` to easily check if an element exists in an array.)
+* The original objects in the `robots` array must **not** be modified, so be sure to use `Object.assign()`
+*/
+const sortedRobots = robots.map((e) => {
+    const object = Object.assign({}, robots);
+    if(knownDecepticons.includes(e.name)){
+      Object.assign(object, { alliance: 'decepticon' });
+    } else {
+      Object.assign(object, { alliance: 'autobot' });
+    }
+    return object;
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +39,18 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+/*
+* Create a new variable called `coloredZebraStripes`;
+* The value of this variable is the mapped `zebraStripes` array;
+* Return each stripe object with the `stripe` filled in with either a `'black'` or `'white'` string.
+* A stripe is black when the stripe's index is even, and it's white if a stripe's index is odd.
+* (Hint: the callback we pass to `.map()` takes more than one parameter)
+* The original objects in the `zebraStripes` array must **not** be modified, so be sure to use `Object.assign()`
+*/
+
+const coloredZebraStripes = zebraStripes.map((e, index) => {
+  var temp = Object.assign({}, e);
+  if(index % 2 === 0) Object.assign(temp, { color: 'black' });
+  else Object.assign(temp, { color: 'white' });
+  return temp;
+});
