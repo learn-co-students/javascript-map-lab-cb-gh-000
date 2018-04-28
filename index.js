4643c8a1fdd3de0abab9aed8efa650f170d3f85a
Cameron Green <cgree78@gmail.com>
2018-04-28 01:19:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
 const knownDecepticons = ['Megatron', 'Skywarp', 'Laserbeak', 'Barricade'];
 
 const robots = [
-  { name: 'Bumblebee', alliance: null },
-  { name: 'Laserbeak', alliance: null },
-  { name: 'Barricade', alliance: null },
-  { name: 'Optimus Prime', alliance: null },
-  { name: 'Skywarp', alliance: null },
-  { name: 'Megatron', alliance: null },
-  { name: 'Ironhide', alliance: null },
-  { name: 'Ratchet', alliance: null }
+  {
+    name: 'Bumblebee',
+    alliance: null
+  }, {
+    name: 'Laserbeak',
+    alliance: null
+  }, {
+    name: 'Barricade',
+    alliance: null
+  }, {
+    name: 'Optimus Prime',
+    alliance: null
+  }, {
+    name: 'Skywarp',
+    alliance: null
+  }, {
+    name: 'Megatron',
+    alliance: null
+  }, {
+    name: 'Ironhide',
+    alliance: null
+  }, {
+    name: 'Ratchet',
+    alliance: null
+  }
 ];
 
 const zebraStripes = [
-  { width: 9.12, color: null },
-  { width: 5.71, color: null },
-  { width: 6.01, color: null },
-  { width: 1.54, color: null },
-  { width: 8.34, color: null },
-  { width: 7.77, color: null },
-  { width: 0.59, color: null },
-  { width: 7.31, color: null }
+  {
+    width: 9.12,
+    color: null
+  }, {
+    width: 5.71,
+    color: null
+  }, {
+    width: 6.01,
+    color: null
+  }, {
+    width: 1.54,
+    color: null
+  }, {
+    width: 8.34,
+    color: null
+  }, {
+    width: 7.77,
+    color: null
+  }, {
+    width: 0.59,
+    color: null
+  }, {
+    width: 7.31,
+    color: null
+  }
 ];
+
+const sortedRobots = robots.map((robot) => {
+  return Object.assign({}, robot, {
+    name: robot.name,
+    alliance: knownDecepticons.includes(robot.name)
+      ? "decepticon"
+      : "autobot"
+  });
+})
+
+
+
+const coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  return Object.assign({}, stripe, {
+    width: stripe.width,
+    color: ((index + 1) % 2 === 0)
+      ? "black"
+      : "white"
+  })
+})
